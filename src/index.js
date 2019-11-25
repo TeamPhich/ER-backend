a4361839fe246c18b97947104ba34812d2d56af0
vansi99 <vansido99@gmail.com>
2019-11-25 15:35:00 +0700

    ok

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const config = require("config");
+const port = config.get("PORT");
 
 const app = express();
 
 app.use("/api/v1", (req, res) => {
     res.send("ER-backend");
+});
+
+app.listen(port, () => {
+    console.log("ER-backend is running on PORT:", port)
 });
 
 module.exports = app;
