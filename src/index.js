72edfe7fa7970c92b7e724d11d8b9668acd68bbc
vansi99 <vansido99@gmail.com>
2019-11-26 01:53:26 +0700

    morgan

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const cors = require("cors");
 const config = require("config");
 const port = config.get("PORT");
 
 const app = express();
+
+morgan.token('remote-addr', function (req) {
+    return req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+});
+
+morgan.token('http-version', function (req) {
+    return ((req.headers['x-forwarded-proto']) ? req.headers['x-forwarded-proto'] : "");
+});
+
+app.disable('x-powered-by');
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan(':remote-addr - [:date[web]] ":method :url HTTP/:http-version" :status :res[content-length]'));
+app.use(bodyParser.urlencoded({
+    limit: '100mb',
+    extended: true
+}));
+app.use(morgan('combined'));
+
 
 app.use("/ER-backend/api/v1", (req, res) => {
     res.send("ER-backend");
