c7c320ba00a14040771d745e6daa271d3e7b7a67
vansi99 <vansido99@gmail.com>
2019-12-02 13:24:20 +0700

    change test

--- test/dev.js
+++ test/dev.js
@@ -12,7 +12,7 @@ describe('health check dev server', () => {
     describe('/GET', () => {
         it('it should return ER-backend', (done) => {
             chai.request(server)
-                .get('/ER-backend/api/v1')
+                .get('/api/v1')
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.text.should.be.a('string');
