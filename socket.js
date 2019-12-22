6a083a07b1d456c9f788645d856f3ac4754a59a7
vansi99 <vansido99@gmail.com>
2019-12-22 19:22:20 +0700

    socket

--- socket.js
+++ socket.js
@@ -10,26 +10,51 @@ const secretkey = config.get("SECRET_KEY");
 const io = require("socket.io")(server);
 
 io.use((socket, next) => {
-    if (socket.handshake.query && socket.handshake.query.spaceToken) {
+    if (socket.handshake.query && socket.handshake.query.token) {
         const {token, exam_id} = socket.handshake.query;
-        if(!exam_id) return next(new Error('missing e'));
-        jwt.verify(token, secretkey, (err, decoded) => {
-            if (err) return next(new Error('Authentication error'));
+        if (!exam_id) return next(new Error('missing group_id'));
+        jwt.verify(token, secretkey, async (err, decoded) => {
+            if (err) {
+                return next(new Error('Authentication error'));
+            }
             socket.tokenData = decoded;
             socket.exam_id = exam_id;
+            const exams = await db.exams.findAll({
+                where: {
+                    id: exam_id
+                }
+            });
+            if(!exams.length) return next(new Error("exam isn't existed"));
+            socket.start_time = exams[0].dataValues.start_time;
+            socket.finish_time = exams[0].dataValues.finish_time;
             next();
         });
     } else {
         next(new Error('Authentication error'));
     }
-}).on("connection", (socket) => {
-    console.log("ok")
+}).on("connection", async (socket) => {
+    let startRegistFlag = false;
+    let finishRegistFlag = false;
+
+    let checkStartTime = setInterval(() => {
+        const now = Date.now();
+        if(socket.start_time >= now){
+            socket.emit("registing.time.start");
+            startRegistFlag = true;
+        }
+        if(now >= socket.finish_time){
+            socket.emit("registing.time.finish");
+            startRegistFlag = false;
+            finishRegistFlag = true;
+            clearInterval(checkStartTime);
+        }
+    }, 1000);
+
 });
 
 server.listen(port, () => {
     console.log("server is running on port: ", port);
 });
 
-
-module.exports = app;
+module.exports = server;
 
