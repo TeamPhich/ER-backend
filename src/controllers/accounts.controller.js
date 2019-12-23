6c089f96bf51612bcfc11c02613800f09193ed71
vansi99 <vansido99@gmail.com>
2019-12-23 15:25:36 +0700

    ok

--- src/controllers/accounts.controller.js
+++ src/controllers/accounts.controller.js
@@ -88,7 +88,108 @@ async function register(req, res) {
     }
 }
 
+async function getInformation(req, res) {
+    const {id} = req.tokenData;
+    try {
+        const accountProfile = await db.accounts.findOne({
+            where: {
+                id
+            },
+            attributes: ["fullname", "birthday", "email", "user_name"]
+        });
+        res.json(responseUtil.success({data: accountProfile}))
+    } catch (err) {
+        res.json(responseUtil.fail({reason: err.message}))
+    }
+}
+
+async function updateProfile(req, res) {
+    try {
+        const {id} = req.tokenData;
+        const {fullname, birthday, email} = req.body;
+        if (!validator.validate(email)) throw new Error("email sai định dạng.");
+        const emailExisted = await db.accounts.findAll({
+            where: {
+                email,
+                id: {
+                    [db.Sequelize.Op.not]: [id]
+                }
+            }
+        });
+        if (emailExisted.length) throw new Error("email đã tồn tại");
+        await db.accounts.update({
+            fullname,
+            birthday,
+            email
+        }, {
+            where: {
+                id
+            }
+        });
+        res.json(responseUtil.success({data: {}}))
+    } catch (err) {
+        res.json(responseUtil.fail({reason: err.message}))
+    }
+}
+
+async function changePassword(req, res) {
+    try {
+        const {oldPassword, newPassword} = req.body;
+        const {id} = req.tokenData;
+        if (!oldPassword) throw new Error("oldPassword field is missing");
+        if (!newPassword) throw new Error("newPassword field is missing");
+
+        let [userData] = await db.accounts.findAll({
+            where: {
+                id
+            }
+        });
+
+        if (!userData) throw new Error("user_name or password is incorrect");
+
+        let user = userData.dataValues;
+        const hashOldPassword = user.password;
+        const checkPass = bcrypt.compareSync(oldPassword, hashOldPassword);
+        if (!checkPass) throw new Error("oldPassword is incorrect");
+        let salt = await bcrypt.genSalt(10);
+        let hashNewPassword = await bcrypt.hash(newPassword, salt);
+        await db.accounts.update({
+            password: hashNewPassword
+        }, {
+            where: {id}
+        });
+        res.json(responseUtil.success({data: {}}))
+    } catch (err) {
+        res.json(responseUtil.fail({reason: err.message}))
+    }
+}
+
+async function resetPassword(req, res) {
+    try {
+        const {mssv} = req.params;
+        if (!mssv) throw new Error("mssv params is missing");
+        const userData = await db.accounts.findAll({
+            user_name: mssv
+        });
+        if (!userData.length) throw new Error("user isn't existed");
+        let salt = await bcrypt.genSalt(10);
+        let hashPassword = await bcrypt.hash(mssv, salt);
+        await db.accounts.update({
+            password: hashPassword
+        }, {
+            where: {user_name: mssv}
+        });
+        res.json(responseUtil.success({data: {}}))
+    } catch (err) {
+        res.json(responseUtil.fail({reason: err.message}))
+    }
+}
+
 module.exports = {
     login,
-    register
+    register,
+    getInformation,
+    updateProfile,
+    changePassword,
+    resetPassword
 };
--- src/routes/accounts.route.js
+++ src/routes/accounts.route.js
@@ -5,6 +5,21 @@ const accountController = require("../controllers/accounts.controller");
 const privilegesMiddleware = require("../middlewares/privileges.middleware");
 
 router.post("/login", accountController.login);
-router.post("/register", tokenMiddleware.verify, privilegesMiddleware.verify(1),accountController.register);
-
+router.post("/register",
+    tokenMiddleware.verify,
+    privilegesMiddleware.verify(1),
+    accountController.register);
+router.get("/profile",
+    tokenMiddleware.verify,
+    accountController.getInformation);
+router.put("/profile",
+    tokenMiddleware.verify,
+    accountController.updateProfile);
+router.put("/passwords",
+    tokenMiddleware.verify,
+    accountController.changePassword);
+router.put("/students/:mssv/passwords",
+    tokenMiddleware.verify,
+    privilegesMiddleware.verify(1),
+    accountController.resetPassword);
 module.exports = router;
